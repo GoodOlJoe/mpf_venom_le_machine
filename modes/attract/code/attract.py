9cb3455dae3740937c77244cae606a08afc40c71
Joe Eggers <jeggers@jackel.net>
2025-05-16 11:15:04 -0400

    add WIP of working step and loop events with parameters from a show

--- modes/attract/code/attract.py
+++ modes/attract/code/attract.py
@@ -1,12 +1,14 @@
 from mpf.core.mode import Mode
 from mpf.core.rgb_color import RGBColor
 
+
 # https://missionpinball.org/latest/code/introduction/mode_code/
 class Attract(Mode):
 
     def mode_init(self):
         self.__start_color = (255, 4, 217)
-        self.__end_color = (81, 255, 0) # blue
+        self.__end_color = (81, 255, 0)  # blue
+        self.__loop_count = 0
 
     def mode_start(self, **kwargs):
 
@@ -18,11 +20,16 @@ class Attract(Mode):
         # what's the player's score?
         # print('Score: {}'.format(self.player.score))
 
-        self.add_mode_event_handler('timer_ndx1111_tick', self.ndx1111_tick_handler)
-        self.add_mode_event_handler('timer_ndx1111_complete', self.ndx1111_complete_handler)
+        self.add_mode_event_handler("attract_light_show_auto_step", self.step_handler)
+        self.add_mode_event_handler("attract_light_show_looped", self.loop_handler)
+        self.add_mode_event_handler("attract_light_show_stopped", self.stop_handler)
+        # self.add_mode_event_handler("timer_ndx1111_tick", self.ndx1111_tick_handler)
+        # self.add_mode_event_handler(
+        #     "timer_ndx1111_complete", self.ndx1111_complete_handler
+        # )
 
         # turn LED "led01" red
-        # self.machine.leds.led01.color('red')        
+        # self.machine.leds.led01.color('red')
 
     def lerp_color(self, color1, color2, amount):
         """
@@ -38,16 +45,28 @@ class Attract(Mode):
         """
         r1, g1, b1 = color1
         r2, g2, b2 = color2
-        
+
         r = int(r1 + (r2 - r1) * amount)
         g = int(g1 + (g2 - g1) * amount)
         b = int(b1 + (b2 - b1) * amount)
-        
+
         return (r, g, b)
-    
+
     # def rgb_to_hex(self,color):
     #     r, g, b = color
     #     return "{:02x}{:02x}{:02x}".format(r, g, b).upper()
+
+    def stop_handler(self, **kwargs):
+        self.__loop_count = 0
+        print("Inside stop handler, loop count is " + str(self.__loop_count))
+
+    def loop_handler(self, **kwargs):
+        self.__loop_count += 1
+        print("Inside loop handler, loop count is " + str(self.__loop_count))
+
+    def step_handler(self, **kwargs):
+        # n = kwargs['l_88_venomized_1'][0]
+        print("Inside step handler, loop count is " + str(self.__loop_count))
 
     def ndx1111_tick_handler(self, **kwargs):
         # print("Inside tick handler")
@@ -58,15 +77,24 @@ class Attract(Mode):
         #     self.machine.variables.set_machine_var("var_from_code","Hello!")
         # else:
         # n = str(kwargs['ticks']) + " / " + str(kwargs['ticks_remaining'])
-        interval = 1 / ( kwargs['ticks'] + kwargs['ticks_remaining'])
-        current_color = self.lerp_color(self.__start_color, self.__end_color, interval * kwargs['ticks'])
-        self.machine.variables.set_machine_var("lerped_color",RGBColor.rgb_to_hex(current_color))
-        self.machine.lights['l_88_venomized_1'].color(RGBColor.rgb_to_hex(current_color))
+        interval = 1 / (kwargs["ticks"] + kwargs["ticks_remaining"])
+        current_color = self.lerp_color(
+            self.__start_color, self.__end_color, interval * kwargs["ticks"]
+        )
+        self.machine.variables.set_machine_var(
+            "lerped_color", RGBColor.rgb_to_hex(current_color)
+        )
+        self.machine.lights["l_88_venomized_1"].color(
+            RGBColor.rgb_to_hex(current_color)
+        )
         # self.machine.lights['l_88_venomized_1'].color('green')
-        self.machine.lights['l_89_venomized_2'].color('purple')
-        self.machine.lights['l_90_venomized_3'].color('purple')
-
+        self.machine.lights["l_89_venomized_2"].color("purple")
+        self.machine.lights["l_90_venomized_3"].color("purple")
 
     def ndx1111_complete_handler(self, **kwargs):
-        self.machine.lights['l_88_venomized_1'].color(RGBColor.rgb_to_hex(self.__end_color))
-        self.machine.variables.set_machine_var("lerped_color",RGBColor.rgb_to_hex(self.__end_color))
+        self.machine.lights["l_88_venomized_1"].color(
+            RGBColor.rgb_to_hex(self.__end_color)
+        )
+        self.machine.variables.set_machine_var(
+            "lerped_color", RGBColor.rgb_to_hex(self.__end_color)
+        )
