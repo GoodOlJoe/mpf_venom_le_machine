1aa2b34092a8e949c6d3d3227b00ec313475eac9
Joe Eggers <jeggers@jackel.net>
2025-05-19 11:48:14 -0400

    add first light show wipe effect with working point-left-of test

--- modes/attract/code/attract.py
+++ modes/attract/code/attract.py
@@ -3,43 +3,123 @@ from mpf.core.mode import Mode
 from ...util.plane import Plane
 from ...util.color import Color
 
+
 # https://missionpinball.org/latest/code/introduction/mode_code/
 # https://colordesigner.io/gradient-generator
 class Attract(Mode):
 
     def mode_init(self):
-        self.__start_color = (255, 4, 217)
-        self.__end_color = (81, 255, 0)  # blue
-        self.__loop_count = 0
-        self.__lerp_interval = 10
+        # region extra bosses light show properties
+        self.__extra_bosses_loop_count = 0
+        self.__extra_bosses_lerp_interval = 10
+        # endregion
+        # region wipe ul to lr light show properties
+        self.__wipe_ul_to_lr_loop_count = 1
+        # how many steps to complete a wipe of the whole playfield
+        self.__wipe_ul_to_lr_steps_per_wipe = 20.0
+        # endregion
 
     def mode_start(self, **kwargs):
+        # region extra bosses light show
+        self.add_mode_event_handler(
+            "attract_extra_bosses_light_show_settings",
+            self.extra_bosses_settings_handler,
+        )
+        self.add_mode_event_handler(
+            "attract_extra_bosses_light_show_auto_step", self.extra_bosses_step_handler
+        )
+        self.add_mode_event_handler(
+            "attract_extra_bosses_light_show_looped", self.extra_bosses_loop_handler
+        )
+        self.add_mode_event_handler(
+            "attract_extra_bosses_light_show_stopped", self.extra_bosses_stop_handler
+        )
+        # endregion
+        # region Wipe UL to LR light show
+        # self.add_mode_event_handler(
+        #     "attract_wipe_ul_to_lr_light_show_settings",
+        #     self.wipe_ul_to_lr_settings_handler,
+        # )
+        self.add_mode_event_handler(
+            "attract_wipe_ul_to_lr_light_show_auto_step",
+            self.wipe_ul_to_lr_step_handler,
+        )
+        self.add_mode_event_handler(
+            "attract_wipe_ul_to_lr_light_show_looped", self.wipe_ul_to_lr_loop_handler
+        )
+        self.add_mode_event_handler(
+            "attract_wipe_ul_to_lr_light_show_stopped", self.wipe_ul_to_lr_stop_handler
+        )
+        # endregion
 
-        # extra bosses light show
-        self.add_mode_event_handler(
-            "attract_extra_bosses_light_show_settings", self.settings_handler
-        )
-        self.add_mode_event_handler(
-            "attract_extra_bosses_light_show_auto_step", self.step_handler
-        )
-        self.add_mode_event_handler(
-            "attract_extra_bosses_light_show_looped", self.loop_handler
-        )
-        self.add_mode_event_handler(
-            "attract_extra_bosses_light_show_stopped", self.stop_handler
-        )
+    # region  Wipe UL to LR Light Show Handlers
+    def wipe_ul_to_lr_step_handler(self, **kwargs):
 
-    def settings_handler(self, **kwargs):
-        self.__lerp_interval = kwargs["lerp_interval"]
+        if self.__wipe_ul_to_lr_loop_count >= self.__wipe_ul_to_lr_steps_per_wipe + 1:
+            self.__wipe_ul_to_lr_loop_count = 1
 
-    def step_handler(self, **kwargs):
+        # this wipe is simple linear so the line's endpoints will always be
+        # n,0 and 0,n
+        increment = 1 / (self.__wipe_ul_to_lr_steps_per_wipe / 2)
+        # sx = sy = ex = ey = increment
+
+        # for i in range( 1,int(self.__wipe_ul_to_lr_steps_per_wipe)+1 ):
+        #     print ( f'{i:{0}>2}:   {round(sx,2):.1f}, {round(sy,2):.1f} ....   {round(ex,2):.1f}, {round(ey,2):.1f}')
+
+        # set increment
+        # sy and ex
+        #     increment
+        #     clamp to 1
+        # sx and ey
+        #     0 until sy >= 1
+        #     then start incrementing
+
+        ex = increment * self.__wipe_ul_to_lr_loop_count
+        if ex > 1.0:
+            ex = 1.0
+        sy = ex
+
+        if sy < 1:
+            ey = 0
+        else:
+            ey = (increment * self.__wipe_ul_to_lr_loop_count) - 1
+        sx = ey
+
+        # this wipe is simple linear so the line's endpoints will always be
+        # n,0 and 0,n
+        # increment = 1 / self.__wipe_ul_to_lr_steps_per_wipe
+        # endpoint = self.__wipe_ul_to_lr_loop_count * increment
+        result = Plane.side(0.4, 0.4, (sx, sy), (ex, ey))
+        # print("    wipe step")
+        # print(f"        loop count {str(self.__wipe_ul_to_lr_loop_count)}")
+        # print(f"        increment  {str(increment)}")
+        # print(
+        #     f"        line       ({round(sx,2):.1f}, {round(sy,2):.1f})    ({round(ex,2):.1f}, {round(ey,2):.1f})"
+        # )
+        # print(f"        side       {result}")
+        # print(f"        is left of {str(-1 == result)}")
+
+    # def wipe_ul_to_lr_settings_handler(self, **kwargs):
+    #     self.__wipe_ul_to_lr_lerp_interval = kwargs["lerp_interval"]
+
+    def wipe_ul_to_lr_loop_handler(self, **kwargs):
+        self.__wipe_ul_to_lr_loop_count += 1
+
+    def wipe_ul_to_lr_stop_handler(self, **kwargs):
+        self.__wipe_ul_to_lr_loop_count = 0
+
+    # endregion
+    # region  Extra Bosses Light Show Handlers
+    def extra_bosses_step_handler(self, **kwargs):
 
         for lerp_light in kwargs["lerp_lights"]:
             # print("    typeof(lerp_light) " + str(type(lerp_light)))
 
-            lerp = float(self.__loop_count % self.__lerp_interval)
+            lerp = float(
+                self.__extra_bosses_loop_count % self.__extra_bosses_lerp_interval
+            )
             if lerp != 0:
-                lerp = 1 / self.__lerp_interval * lerp
+                lerp = 1 / self.__extra_bosses_lerp_interval * lerp
 
             # the lerp_light parameter is an list containing first the light name followed
             # by any number of colors that serve as "stops" on the lerp
@@ -49,8 +129,13 @@ class Attract(Mode):
 
             self.machine.lights[lerp_light[0]].color(lerp_color)
 
-    def loop_handler(self, **kwargs):
-        self.__loop_count += 1
+    def extra_bosses_settings_handler(self, **kwargs):
+        self.__extra_bosses_lerp_interval = kwargs["lerp_interval"]
 
-    def stop_handler(self, **kwargs):
-        self.__loop_count = 0
+    def extra_bosses_loop_handler(self, **kwargs):
+        self.__extra_bosses_loop_count += 1
+
+    def extra_bosses_stop_handler(self, **kwargs):
+        self.__extra_bosses_loop_count = 0
+
+    # endregion
--- modes/util/plane.py
+++ modes/util/plane.py
@@ -6,21 +6,50 @@ from shapely.ops import split
 class Plane:
 
     @staticmethod
-    def left_of(pointX, pointY, lineStart, lineEnd):
+    def side(pointX, pointY, lineStart, lineEnd):
         """
 
-        Return true if the given point x,y is LEFT OF or ON a line intersecting
-        a plane bounded by 0,1 with both X and Y origins in upper left.
-        Otherwise return false
+        Return a value indicating if the given point x,y is LEFT, RIGHT, or ON a
+        line intersecting a plane bounded by 0,1 with both X and Y origins in
+        upper left.
+
+        -1 = point is LEFT of line
+         0 = point is ON of line
+         1 = point is RIGHT of line
+
+         If the point is not contained in the playfield at all (that is, if it's
+         not in a plane bounded by 0,1) then the return result is unreliable
 
         """
         lineStartX, lineStartY = lineStart
         lineEndX, lineEndY = lineEnd
         playfield = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
         line = LineString([(lineStartX, lineStartY), (lineEndX, lineEndY)])
-        result = split(playfield, line) # split the playfield on the line, returning two polygons
-        return shapely.contains(playfield, Point(pointX, pointY))
+        # split the playfield on the line, returning two polygons (or one if line doesn't intersect playfield)
+        split_result = split(playfield, line)
+        # print("            point           " + str(Point(pointX, pointY)))
+        # print("            split result    " + str(split_result))
+        # print("            len(split_result.geoms) " + str(len(split_result.geoms)))
 
+        left_poly_contains_point = shapely.contains(
+            split_result.geoms[0], Point(pointX, pointY)
+        )
+
+        right_poly_contains_point = False
+        if len(split_result.geoms) > 1:
+            right_poly_contains_point = shapely.contains(
+                split_result.geoms[1], Point(pointX, pointY)
+            )
+
+        if left_poly_contains_point:
+            # print("            point is LEFT of the line")
+            return -1
+        elif right_poly_contains_point:
+            # print("            point is RIGHT of the line")
+            return 1
+        else:
+            # print("            point is ON the line")
+            return 0
 
     @staticmethod
     def bounded(box_ulX, box_ulY, box_lrX, box_lrY, x, y):
